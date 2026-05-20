a58b6e62a78ffb5cbbdbae5b830cf07054fdfb0a
Leone Gomes <batistaleoneg@gmail.com>
2026-05-20 20:02:51 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// configuração do vite para usar o plugin do React
 export default defineConfig({
   plugins: [react()],
 })
